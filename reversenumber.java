e6f7dc65513b6df4bb51cf7e99250d4aadc2daa7
TharaneeshJ <tharaneeshj@gmail.com>
2025-11-26 08:05:32 +0530

    Add program to reverse a number

---
+++ reversenumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reversenumber {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number to reverse:");
+        int number = scanner.nextInt();
+        int reversedNumber = 0;
+        while (number==0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+            System.out.println("Reversed Number is: " + reversedNumber);
+            scanner.close();
+        }
+    }
+}
